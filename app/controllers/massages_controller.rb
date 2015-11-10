62a4286858653bb778f48ee0fc46b84caf383d9a
hrnee <blue-yg.spsou@gmail.com>
2015-11-10 20:19:53 +0900

    edit message_board

--- app/controllers/massages_controller.rb
+++ app/controllers/massages_controller.rb
@@ -4,21 +4,8 @@ class MassagesController < ApplicationController
   # GET /massages
   # GET /massages.json
   def index
-    @massages = Massage.all
-  end
-
-  # GET /massages/1
-  # GET /massages/1.json
-  def show
-  end
-
-  # GET /massages/new
-  def new
     @massage = Massage.new
-  end
-
-  # GET /massages/1/edit
-  def edit
+    @massages = Massage.all
   end
 
   # POST /massages
@@ -26,46 +13,12 @@ class MassagesController < ApplicationController
   def create
     @massage = Massage.new(massage_params)
 
-    respond_to do |format|
-      if @massage.save
-        format.html { redirect_to @massage, notice: 'Massage was successfully created.' }
-        format.json { render :show, status: :created, location: @massage }
-      else
-        format.html { render :new }
-        format.json { render json: @massage.errors, status: :unprocessable_entity }
-      end
+    if @massage.save
+      redirect_to massages_url, notice: "Message was successfully created."
+    else
+      redirect_to massages_url, alert: "Message was unsuccessfully created."
     end
   end
-
-  # PATCH/PUT /massages/1
-  # PATCH/PUT /massages/1.json
-  def update
-    respond_to do |format|
-      if @massage.update(massage_params)
-        format.html { redirect_to @massage, notice: 'Massage was successfully updated.' }
-        format.json { render :show, status: :ok, location: @massage }
-      else
-        format.html { render :edit }
-        format.json { render json: @massage.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /massages/1
-  # DELETE /massages/1.json
-  def destroy
-    @massage.destroy
-    respond_to do |format|
-      format.html { redirect_to massages_url, notice: 'Massage was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_massage
-      @massage = Massage.find(params[:id])
-    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def massage_params
